32b79364ff8a6f1893b256aff4354c30052b3bd9
Oscar Olsson <pinnenolsson@hotmail.com>
2020-05-09 16:11:07 +0200

    Uppdatering av projektupplägg.

---
+++ functions.c
@@ -0,0 +1,18 @@
+#include "functions.h"
+#include "stdio.h"
+
+
+void problem_1() {
+
+	for (int i = 0; i < 10; i++) {
+
+		if (i % 3 == 0) {
+			printf("%d\n", i);
+		}
+		else if (i % 5 == 0) {
+			printf("%d\n", i);
+		}
+
+	}
+
+}
---
+++ functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void problem_1();
+
+
+#endif
+
