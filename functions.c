c9ae47bb41beef7084652329ae12b812fe871a86
Oscar Olsson <pinnenolsson@hotmail.com>
2020-05-09 16:16:44 +0200

    Problem 1: Multiplies of 3 and 5 lösning.

--- functions.c
+++ functions.c
@@ -4,15 +4,18 @@
 
 void problem_1() {
 
-	for (int i = 0; i < 10; i++) {
+	int sum = 0;
 
+	for (int i = 0; i < 1000; i++) {
 		if (i % 3 == 0) {
-			printf("%d\n", i);
+			sum += i;
 		}
 		else if (i % 5 == 0) {
-			printf("%d\n", i);
+			sum += i;
 		}
 
 	}
+
+	printf("%d", sum);
 
 }
